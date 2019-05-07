ce3817ef4070e51bece8eb83bf8861112e867a4b
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-07 15:49:14 -0500

    updatedOne and updateMany

--- mongodb.js
+++ mongodb.js
@@ -10,44 +10,25 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{//this
     }
 
     const db =  client.db(databaseName)
-    //db.collection('users').findOne({name:"Erick",age:25},(error,user)=>{
-    //searching only one document
-        db.collection('users').findOne({_id:new ObjectID('5cd1ad44d9865c18bf5a72a3')},(error,user)=>{
-            //we specify new ObjectID("...") because what is stored as an id is not the string but an object.
-        if(error){
-            return console.log("Unable to fetch")
-        }
-        console.log(user)
+    //updating one element
+    // db.collection('users').updateOne(
+    //     {_id: new ObjectID('5cd19d0a3a138c52348a026c')},//the id to update
+    //     //{$set:{name: 'David'}}//the values to update
+    //     {$inc:{age:-1}} //increments a value in a specified amount
+    //     //for example here we are decreasing in one.
+    // ).then(result=>{
+    //     console.log(result)
+    // }).catch(e=>{
+    //     console.log(e)
+    // })
+
+    //update many
+    db.collection('tasks').updateMany(
+        {completed:false},//search criteria
+        {$set:{completed:true}} //what to update
+    ).then(result=>{
+        console.log(result)
+    }).catch(e=>{
+        console.log(e)
     })
-
-    db.collection('tasks').findOne({_id:new ObjectID('5cd1a3b71cdf9c6dd1ed3d5c')},(error,task)=>{
-            //we specify new ObjectID("...") because what is stored as an id is not the string but an object.
-        if(error){
-            return console.log("Unable to fetch")
-        }
-        console.log(task)
-    })
-
-    //seacrching multiple documents
-    db.collection('users').find({age:27}).toArray((error,users)=>{
-        if(error){
-            return console.log('Error')
-        }
-        console.log(users)
-    })//instead a callback it is returned a cursor which
-    //is a pointer to the data in the database; 
-    //by using toArray we pull all the elementes that matches the search criteria
-    //we must be sure we have enough ram.
-
-    db.collection('users').find({age:27}).count((error,count)=>{
-        console.log(count)
-    })//instead of pull all only for counting operation we let mongo do the search
-    //so we do not put pressure on our ram memory.
-
-    db.collection('tasks').find({completed:false}).toArray((error,tasks)=>{
-        if(error){
-            return console.log('Error')
-        }
-        console.log(tasks)
-    })    
 })
