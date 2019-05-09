75a7fb07b5bfae5ca45d45a77797aaedb8c4c80c
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-09 17:04:18 -0500

    tested promise chaining 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,5 +4,5 @@ mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`,{
     useNewUrlParser:true,
     useCreateIndex: true, //when mongoose works with mongodb our index is created allowing us to 
     //quickly access the data we need to access.
-    useFindAndModify:false //to handle DeprecationWarning: collection.findAndModify is deprecated
+    useFindAndModify:false //to handle DeprecationWarning: collection.findAndModify is deprecated (see lecture 93)
 })
