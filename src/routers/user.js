06c67d1cdea4eebf5392dd72669488fb39613064
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-15 13:06:33 -0500

    restructured all user routes => user/me and added authentication to all of them

--- src/routers/user.js
+++ src/routers/user.js
@@ -67,25 +67,7 @@ router.get('/users/me',auth,async (req,res)=>{
     // }    
 })
 
-router.get('/users/:id',async(req,res)=>{ //:id: placeholder; dynamic route handler
-    //req.params gives all the params that comes into the query
-    //console.log(req.params) //{ id: 'bkdsbfaksdbfkjasd' }
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if(!user){//means mongo does not find the user with that id
-            res.status(404).send()
-        }else{
-            res.send(user)
-        }
-    }catch(e){
-        res.status(500).send()
-    }
-})
-
-
-
-router.patch("/users/:id", async(req,res)=>{
+router.patch("/users/me", auth ,async(req,res)=>{
     const updates = Object.keys(req.body) //array of properties of the object
     const allowedUpdates = ['name','email','password','age']
     const isValidOperation = updates.every(update=>allowedUpdates.includes(update))//shorthand form
@@ -98,17 +80,13 @@ router.patch("/users/:id", async(req,res)=>{
     try{
 
         //const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true}) //updates bypassing mongoose
-        const user = await User.findById(req.params.id)
-        updates.forEach(update=>user[update]=req.body[update])
+        //const user = req.user//await User.findById(req.user._id)
+        updates.forEach(update=>req.user[update]=req.body[update])
 
-        await user.save()
+        await req.user.save()
         //{new:true} => returns the new user; runValidators:true => the program checks requirements of the model restrictions
         //instead of the old one.
-        if(!user){
-            res.status(404).send()
-        }else{
-            res.send(user)
-        }
+        res.send(req.user)       
 
     }catch(e){
         res.status(500).send(e)  //bad id, could not connect to db.
@@ -116,14 +94,15 @@ router.patch("/users/:id", async(req,res)=>{
 })
 
 
-router.delete('/users/:id',async (req,res)=>{
+router.delete('/users/me',auth,async (req,res)=>{
     try{
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user){
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        // const user = await User.findByIdAndDelete(req.user._id) //auth middleware attached the id
+        // if(!user){
+        //     return res.status(404).send()
+        // }
+        await req.user.remove() //achieves the exact same behaviour of lines of code above;
+        //remove is a mongoose method
+        res.send(req.user)
     }catch(e){
         res.status(500).send()
     }
