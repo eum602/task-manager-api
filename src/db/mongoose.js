5598aa380774705ba2db77cb19437a1ea11360b8
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-18 12:41:46 -0500

    added handler error to mongoose connection to db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 //const dbName = `task-manager-api`
+//mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useCreateIndex: true, //when mongoose works with mongodb our index is created allowing us to 
     //quickly access the data we need to access.
     useFindAndModify:false //to handle DeprecationWarning: collection.findAndModify is deprecated (see lecture 93)
-})
+}).then(()=>{console.log('Connected to db')}).catch(e=>{console.log('Error:',e)})
