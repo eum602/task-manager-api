5a4d27438afe1f8e0ca37c5660b4c81775033f2b
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-23 13:47:52 -0500

    tested login and used beforeEach to empty db before each test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,22 @@
 const request=require('supertest')
 
 const app =  require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Arturo',
+    email: 'r2d2@gmail.com',
+    password:'123456789'
+}
+
+beforeEach(async()=>{
+    await User.deleteMany() //emptying database
+    await new User(userOne).save() //creating a new user
+})
+
+// afterEach(()=>{
+//     console.log('afterEach')
+// })
 
 test('Should signup a new user',async()=>{
     await request(app).post('/users').send({
@@ -10,3 +26,16 @@ test('Should signup a new user',async()=>{
     }).expect(201) //res status code expected
 })
 
+test('Should login existing user',async()=>{
+    await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:'123456789'
+    }).expect(200)
+})
+
+test('Should not login nonexistent user',async()=>{
+    await request(app).post('/users/login').send({
+        email:userOne.email,
+        password:'fakepassword'
+    }).expect(400)
+})
