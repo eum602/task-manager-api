de65c2bef67bcd049779bbccbae81a3f1144491e
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-24 17:45:45 -0500

    added tests for tasks; added --runInBand to package json to run tests sequencially

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,23 +1,13 @@
 const request=require('supertest')
-const jwt =  require('jsonwebtoken')
-const mongoose = require('mongoose')
 const app =  require('../src/app')
 const User = require('../src/models/user')
-const userOneId = new mongoose.Types.ObjectId()
-const userOne = {
-    _id : userOneId,
-    name: 'Arturo',
-    email: 'r2d2@gmail.com',
-    password:'123456789',
-    tokens: [{
-        token:jwt.sign({_id: userOneId},process.env.JWT_SECRET)
-    }]
-}
 
-beforeEach(async()=>{
-    await User.deleteMany() //emptying database
-    await new User(userOne).save() //creating a new user
-})
+const {userOneId,userOne,setupDatabase} = require('./features/db')
+
+beforeEach(setupDatabase)
+    //async()=>{
+    //await setupDatabase()    
+//})
 
 // afterEach(()=>{
 //     console.log('afterEach')
