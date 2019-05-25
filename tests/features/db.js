6b22376e85c6a32fed9b5a96c32e7202672d1301
Erick Pacheco Pedraza <eum602@gmail.com>
2019-05-24 22:10:57 -0500

    added more task tests

--- tests/features/db.js
+++ tests/features/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
     _id : userOneId,
@@ -11,12 +12,47 @@ const userOne = {
         token:jwt.sign({_id: userOneId},process.env.JWT_SECRET)
     }]
 }
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id : userTwoId,
+    name: 'Erick',
+    email: 'eum602@gmail.com',
+    password:'123456789!',
+    tokens: [{
+        token:jwt.sign({_id: userTwoId},process.env.JWT_SECRET)
+    }]
+}
 
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: "First task",
+    completed: false,
+    owner: userOneId //userOneId is identical the same than userOne._id
+}
+
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: "Second task",
+    completed: true,
+    owner: userOneId //userOneId is identical the same than userOne._id
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: "Third task",
+    completed: false,
+    owner: userTwo._id //userOneId is identical the same than userOne._id
+}
 const setupDatabase = async () => {
     await User.deleteMany() //emptying database
+    await Task.deleteMany()
     await new User(userOne).save() //creating a new user
+    await new User(userTwo).save() //creating a new user
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
-    userOneId,userOne,setupDatabase
+    userOneId,userOne,userTwo,taskOne,setupDatabase
 }
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const {userOne, setupDatabase} = require('./features/db')
+const {userOne, userTwo ,taskOne, setupDatabase} = require('./features/db')
 beforeEach(setupDatabase)
 
 test('Should create task for user',async()=>{
@@ -17,3 +17,47 @@ test('Should create task for user',async()=>{
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)//toBe(false)
 })
+
+test('Should fetch user tasks', async()=>{
+    const response = await request(app)
+    .get('/tasks')
+    .set('Authorization',`Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+    expect(response.body.length).toEqual(2) //because we are testing with two tasks in the database
+    //you can verify this by checking what the endpoint (GET)tasks/ is responding
+})
+
+test('Should not delete other users task', async()=>{
+    const response = await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization',`Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
+})
+
+//
+// User Test Ideas
+//
+// Should not signup user with invalid name/email/password
+// Should not update user if unauthenticated
+// Should not update user with invalid name/email/password
+// Should not delete user if unauthenticated
+
+//
+// Task Test Ideas
+//
+// Should not create task with invalid description/completed
+// Should not update task with invalid description/completed
+// Should delete user task
+// Should not delete task if unauthenticated
+// Should not update other users task
+// Should fetch user task by id
+// Should not fetch user task by id if unauthenticated
+// Should not fetch other users task by id
+// Should fetch only completed tasks
+// Should fetch only incomplete tasks
+// Should sort tasks by description/completed/createdAt/updatedAt
+// Should fetch page of tasks
